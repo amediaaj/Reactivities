1293a4867ee551f07eb5a9c10cfdf4adb872552f
Alex Amedia <aamedia@asu.edu>
2025-03-12 10:42:33 -0700

    End of section 7

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,9 +5,10 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import App from './app/layout/App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { RouterProvider } from 'react-router';
+import { router } from './app/router/Routes.tsx';
 
 const queryClient = new QueryClient();
 
@@ -17,7 +18,7 @@ createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools />
     </QueryClientProvider>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>,
 )
